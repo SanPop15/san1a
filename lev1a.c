28ab286366b2b18ae9e0b0514ffbfe7e31c148c6
SanPop15 <36375805+SanPop15@users.noreply.github.com>
2019-02-18 15:53:44 +0530

    Create lev1a.c

---
+++ lev1a.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b;
+pritnf("Enter any number");
+scanf("%d",&a);
+if(a>=0)
+{
+printf("Positive");
+}
+if(a<=0)
+{
+printf("Negative");
+}
+if(a==0)
+{
+printf("Zero");
+}
+getch();
+}
