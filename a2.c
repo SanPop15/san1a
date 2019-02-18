36c25583b68ba6124044636a03fb83008c634170
SanPop15 <36375805+SanPop15@users.noreply.github.com>
2019-02-18 16:10:45 +0530

    Create a2.c

---
+++ a2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("Enter number");
+scanf("%d",&a);
+if(a%2==0)
+{
+printf("Even");
+}
+if(a%2==1)
+{
+printf("Odd");
+}
+if((a%2!=0)||(a%2!=1)
+{
+printf("Invalid");
+}
+getch();
+}
